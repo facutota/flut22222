c6d779ad1b03e6a88c17076853d27c5be122f781
Facutota <facutota@yahoo.com.ar>
2023-02-28 11:03:10 -0300

    Agregando Text

---
+++ lib/Widgets/my_icon.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyIcon extends StatelessWidget {
+  const MyIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return Icon(Icons)
+}
