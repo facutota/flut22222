1be919e188e4b4652a5e3f31f43219d87b0330c5
Facutota <facutota@yahoo.com.ar>
2023-02-28 09:27:53 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flut22222/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flut22222
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Widgets/my_text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyText extends StatelessWidget {
+  const MyText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return const Text(
+      "Hola Mundo",
+      style: TextStyle(
+        color: Colors.red,
+      ),
+    );
+  }
+}
