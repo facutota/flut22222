b195a73355af22fc1754e1720ca48e732e936519
Facutota <facutota@yahoo.com.ar>
2023-03-07 12:01:53 -0300

    Actividad

---
+++ lib/Widgets/MultiChild/my_column.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(children: [ ],)
+  }
+}
